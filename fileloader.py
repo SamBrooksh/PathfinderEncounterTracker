e7960c9f88d4f4ee4677ce3b8251135d934b8940
SamBrooksh <sambrooksh@gmail.com>
2024-11-26 16:33:00 -0700

    Modifying some of the objects

--- fileloader.py
+++ fileloader.py
@@ -3,8 +3,33 @@ from keybuilder import load_keys, get_spacing
 from pprint import pp
 from enum import Enum
 from abc import ABC
+from random import randint
 
 object_dict = {}
+basis = {}
+
+class DamageType:
+    def __init__(self, default:Enum, amount: str, d_type=None) -> None:
+        self.amount = amount
+        if d_type is not None:
+            temp = False
+            for j in basis['DamageRoll']['DamageType']['TYPE']:
+                if d_type.upper() == j.name.upper():
+                    self.d_type = j  
+                    temp = True
+                    return
+            if not temp:
+                print(f"Invalid ENUM value! Should be {basis['DamageRoll']['DamageType']['TYPE']}, Found {d_type}")
+                raise KeyError
+        else:
+            self.d_type = default
+    def __call__(self, *args: Any, **kwds: Any) -> tuple[int, Enum]:
+        if 'd' in self.amount:
+            pass
+            #Roll then give result
+        else:
+            return int(self.amount), self.d_type
+
 
 def register(cls):
     object_dict[cls.__name__.lower()] = cls
@@ -13,20 +38,57 @@ def register(cls):
 class BaseObject(ABC):
     def __call__(self, *args: Any, **kwds: Any) -> Any:
         pass
+class DictObject(BaseObject):
+    def __init__(self, start_dict: dict) -> None:
+        self.dict_object = start_dict
 
 @register
-class AttackObject(BaseObject):
+class AttackObject(DictObject):
     def __init__(self, start_dict: dict):
+        super().__init__(start_dict)
         print("Making Attack Object")
+        self.dict_object = start_dict
+
+    def __call__(self, *args: Any, **kwds: Any) -> tuple[int, list[DamageType]]:
+        #Returns the roll, and the damage if hit - the breakdown
+        if 'DamageRoll' in self.dict_object and 'BaseType' in self.dict_object:
+            return self.dict_object['DamageRoll'](self.dict_object['BaseType'])
+        return super().__call__(*args, **kwds)
 
 @register
 class DamageRollObject(BaseObject):
     def __init__(self, start_string: str):
-        print("Making Damage Roll Object")
+        #Need to make Roll
+        self._damage_rolls = []
+        adds_string = start_string[0]['Value'].split('+')
+        for sub in adds_string:
+            temp = sub.split('#')#Use the # to identify the different types I think?
+            if len(temp) > 1:
+                self._damage_rolls.append(DamageType(None, temp[0], temp[1]))
+            else:
+                self._damage_rolls.append(DamageType(None, temp[0]))#Change None to the default somehow
+        #print("Making Damage Roll Object")
+    
+    def __str__(self) -> str:
+        string = f"<Damage Roll Object: Rolls: "
+        for rolls in range(len(self.count)):
+            string += f'{self.count[rolls]}d{self.die} '
+        string += f'Bonus: {self.bonus}>'
+
+    def __repr__(self) -> str:
+        return str(self)        
+
+    def __call__(self, *args: Any, **kwds: Any) -> list[tuple[int, Enum]]:
+        roll = []
+        for rolls in self.count:
+            for _ in range(rolls):
+                roll.append(randint(1, self.die))
+        return rolls, self.bonus
 
 @register
 class ArmyKillsObject(BaseObject):
     def __init__(self, start_dict: dict):
+        super().__init__(start_dict)
         print("Making Army Kills")
 
 def object_creator(typ: str, *args)->BaseObject:
@@ -45,8 +107,8 @@ def sub_basis(basis: dict, keys: list)->tuple[bool, dict]:
 
 
 def build_tree(file:str="samplefile.txt")->dict:
+    global basis
     basis = load_keys()
-    tree = {}
     with open(file, 'r') as fin:
         spacing = [0]   #Track Spacing
         result = {} # Essentially Place each Character into this
